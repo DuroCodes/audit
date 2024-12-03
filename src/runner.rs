7a6a52becf8dfca8f32fac73ccfe7a0189d6e1de
DuroCodes <davidwright13503@gmail.com>
2024-12-03 17:34:18 -0500

    feat: make parsing more flexible to support different formats

--- src/runner.rs
+++ src/runner.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::{
     io::{self, BufRead, BufReader},
     process::{Command, Stdio},
@@ -28,22 +29,31 @@ pub fn from_command(args: &[String]) -> Result<Vec<DayOutput>, String> {
 }
 
 fn parse_output<R: BufRead>(reader: R) -> Result<Vec<DayOutput>, String> {
+    let day_re = Regex::new(r"Day\s+(\d+)").unwrap();
+    let part1_re = Regex::new(r"Part\s+1:\s+(.+)").unwrap();
+    let part2_re = Regex::new(r"Part\s+2:\s+(.+)").unwrap();
+
     let mut results = Vec::new();
     let mut current_day = 0;
     let mut part1 = None;
     let mut part2 = None;
 
     for line in reader.lines().filter_map(Result::ok) {
-        let parts = line.split_whitespace().collect::<Vec<_>>();
-        match parts.as_slice() {
-            ["Day", d] => {
+        match (
+            day_re.captures(&line),
+            part1_re.captures(&line),
+            part2_re.captures(&line),
+        ) {
+            (Some(cap), _, _) => {
                 if current_day > 0 {
                     results.push((current_day, part1.take(), part2.take()));
                 }
-                current_day = d.parse().map_err(|e| format!("Failed to parse day: {e}"))?;
+                current_day = cap[1]
+                    .parse()
+                    .map_err(|e| format!("Failed to parse day: {e}"))?;
             }
-            ["Part", "1:", actual] => part1 = Some(actual.to_string()),
-            ["Part", "2:", actual] => part2 = Some(actual.to_string()),
+            (_, Some(cap), _) => part1 = Some(cap[1].to_string()),
+            (_, _, Some(cap)) => part2 = Some(cap[1].to_string()),
             _ => continue,
         }
     }
